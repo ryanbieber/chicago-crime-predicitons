dcb1ffb75c2577701b7ea9465a78eba855c894e4
Carnufex <53710182+ryanbieber@users.noreply.github.com>
2020-11-01 12:44:05 -0600

    some initial EDA

--- src/model.R
+++ src/model.R
@@ -1,4 +1,64 @@
 ## the modelling application
-source("parameters.R")
+source("~/R/chicago-crime-predicitons/src/parameters.R")
 
 
+library(sparklyr)
+library(dplyr)
+
+sc <- spark_connect(master = "local")
+fbi_codes <- copy_to(sc, fbi_merge_code)
+
+spark_config_settings()
+
+crime <- spark_read_csv(sc, "C:/Users/leroy/Documents/R/chicago-crime-predicitons/data/Crimes_-_2001_to_Present.csv") %>%
+  mutate(FBI_Code = ifelse(is.na(FBI_Code),"MISSING",FBI_Code)) %>%
+  mutate( Location = ifelse(is.na(Location),"MISSING", Location)) %>%
+  mutate(Latitude = ifelse(is.na(Latitude),0, Latitude)) %>%
+  mutate(Longitude = ifelse(is.na(Longitude),0, Longitude)) %>%
+  mutate(Location = ifelse(is.na(Location), "MISSING", Location)) %>%
+  mutate(Location_Description = ifelse(is.na(Location_Description),"MISSING", Location_Description)) %>%
+  inner_join(fbi_merge_code, copy = TRUE) %>%
+  mutate(Month = substr(Date,0,1)) %>%
+  mutate(Day = substr(Date,3,4))
+
+
+
+crime_rename <- crime %>%
+  group_by(Location_Description) %>%
+  tally() %>%
+  mutate(Location_Description = ifelse(n<1000, "LESS THAN 1K", Location_Description))
+
+crime_rename <- collect(crime_rename) %>%
+  group_by(Location_Description) %>%
+  summarise(total = sum(n))
+
+#looking at locations
+# location_unique <- crime %>%
+#   group_by(Location_Description) %>%
+#   tally()
+# location_unique <- collect(location_unique) %>%
+#   arrange(desc(n))
+
+crime %>%
+  group_by(Arrest) %>%
+  tally()
+crime %>%
+  group_by(Domestic) %>%
+  tally()
+
+crime %>%
+  group_by(Arrest, Domestic) %>%
+  tally()
+
+crimes_over_time <- crime %>%
+  group_by(Year) %>%
+  tally()
+
+crime_time <- collect(crimes_over_time) %>%
+  arrange(desc(Year))
+
+crime_update <- crime %>%
+  mutate(Day = lubridate::wday(Date))
+
+
+spark_disconnect(sc)
--- src/parameters.R
+++ src/parameters.R
@@ -1,3 +1,9 @@
 ## put the parameters you want hard coded into the program in this one
 source("functions.R")
 
+Crimes <- c("Homicide 1 and 2","Criminal Sexual Assualt","Robbery","Aggravated Assualt","Aggravated Battery","Burglary","Larceny","Motor Vehicle Theft","Arson",
+                 "Involuntary Manslaugherter", "Simple Assualt","Simple Battery","Forgery and Counterfeiting","Fraud","Embezzlement","Stolen Property","Vandalism","Weapons Violation","Prostitution","Criminal Sexual Abuse",
+                 "Drug Abuse","Gambling","Offenses Against Family","Liquor License","Disorderly Conduct","Misc Non Index Offense")
+FBI_Code <- c("01A","02","03","04A","04B","05","06","07","09","01B","08A","08B","10","11","12","13","14","15","16","17","18","19","20","22","24","26")
+
+fbi_merge_code <- cbind.data.frame(Crimes, FBI_Code)
